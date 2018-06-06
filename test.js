483ab24f2faf19215741ea7b9bc882d5aedcdf36
Priyank Patel <priyankp390@gmail.com>
2018-06-06 23:14:59 +0000

    lib: move uplevel.js to lib directory.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
-const Uplevel = require('./uplevel');
+const Uplevel = require('./lib/uplevel');
 
 const testDBPath = path.resolve(__dirname, 'test-db');
 function rmTestDB() {
